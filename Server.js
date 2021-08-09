38f6b2067ede4d4235563cb2cf69e7acfbb9d0bd
Odeh <abuzaid163@hotmail.com>
2021-08-09 17:11:49 +0300

    Weather Data

---
+++ Server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const server = express();
+const CORS = require('cors');
+
+require('dotenv').config();
+server.use(CORS());
+
+const jSonWeatherDS = require('./data/weather.json');
+
+server.get('/', (req, res) => {
+      res.send('am a Live .. : )');
+});
+
+server.get('/weather/:lon/:lat/:searchQuery', (request, response) => {
+      const foundData = jSonWeatherDS.find(
+            (cityData) =>
+                  +cityData.lon === +request.params.lon &&
+                  +cityData.lat === +request.params.lat &&
+                  cityData.city_name === request.params.searchQuery
+      );
+      if (foundData) {
+            response.send(
+                  foundData.data
+                        .map(daily =>
+                              (new Forecast(daily))))
+      }
+      else { response.status(500).send('No results found!!!'); }
+});
+
+
+class Forecast {
+      constructor(city) {
+            this.date = city.valid_date,
+                  this.description = city.weather.description
+
+      }
+}
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+      console.log(`################################ ~ Running on port ${PORT} ~ ################################`);
+});
