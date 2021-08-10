aac4bdbc483771d8d419184c463630f9c5721fcd
Odeh <abuzaid163@hotmail.com>
2021-08-10 22:45:44 +0300

    lab8-WeatherData

--- Server.js
+++ Server.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const server = express();
 const CORS = require('cors');
+const axios = require('axios');
+const jSonWeatherDS = require('./data/weather.json');
 
 require('dotenv').config();
 server.use(CORS());
 
-const jSonWeatherDS = require('./data/weather.json');
 
-server.get('/', (req, res) => {
-      res.send('am a Live .. : )');
-});
+const configs = {
+      weather_API_key: process.env.WEATHER_API_KEY,
+      movies_API_key : process.env.MOVIE_API_KEY,
+      weather_API_URL: 'http://api.weatherbit.io/v2.0/forecast/daily/',
+      movies_API_URL: 'https://api.themoviedb.org/3/movie/top_rated',
+      port : process.env.PORT 
+}
 
+server.get('/', (request,response) =>   response.send('am a Live .. : )'))
 server.get('/weather/:lon/:lat/:searchQuery', (request, response) => {
       const foundData = jSonWeatherDS.find(
             (cityData) =>
-            ~~+cityData.lon === ~~+request.params.lon &&
-            ~~+cityData.lat === ~~+request.params.lat &&
+                  ~~+cityData.lon === ~~+request.params.lon &&
+                  ~~+cityData.lat === ~~+request.params.lat &&
                   cityData.city_name === request.params.searchQuery
       );
       if (foundData) {
@@ -23,20 +29,33 @@ server.get('/weather/:lon/:lat/:searchQuery', (request, response) => {
                   foundData.data
                         .map(daily =>
                               (new Forecast(daily))))
-                              console.log(`>>>>>>>> Sent ${Date.now()}`)
+            console.log(`>>>>>>>> Sent ${Date.now()}`)
       }
       else { response.status(500).send('No results found!!!'); }
+});
+server.get('/weather/:lon/:lat', (request, response) => {
+      const url =
+      `${configs.weather_API_URL}?key=${configs.weather_API_key}&lang=en&lat=${request.params.lat}&lon=${request.params.lon}`;
+axios
+      .get(url)
+      .then((apiResponse) => {
+            const weatherForcastData = apiResponse.data.data.map((day) => (new Forecast(day)))
+            response.send(weatherForcastData.splice(0, 6))
+            console.log(`>>>>>>>> Sent ${Date.now()}`)
+      })
+      .catch((error) => { response.status(500).send(error) })
 });
 
 
 class Forecast {
       constructor(day) {
-            this.ForcastDate = day.valid_date,
-                  this.description = day.weather.description
+            this.forcastDate = day.datetime;
+            this.description = day.weather.description
       }
 }
 
-const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
-      console.log(`#################### ~ Running on port ${PORT} ~ ####################`);
-});
+
+server.listen(configs.port, () => console.log(`#################### ~ Running on port ${configs.port} ~ ####################`));
+      
+      
+      
